6240a7e30429fcee66705f3bb8b0ba1bd3a08159
emmerigrooms <x41c426@msu.montana.edu>
2024-11-17 14:33:48 -0700

    fixing port number

--- components/GitHub.js
+++ components/GitHub.js
@@ -4,7 +4,7 @@ export default function GitHub(){
         <div>
             <p>I want to do my final project on hashing passwords and making a user database.</p>
             <p>My group and I plan to do this by having users create an account and make a password and then log in on a different page.</p>
-            <p>Here's my <a href="https://github.com/emmerigrooms/first-nextjs">repo</a> for the homework.</p>
+            <p>Here's my <a href="https://github.com/emmerigrooms/reactJS">repo</a> for the homework.</p>
         </div>
     );
 }
